4e6051e2b5b878477d15545757505751039aba6f
Mateo <me@mateo.email>
2016-03-08 11:15:33 -0800

    finally you lil shit

--- js/age/max.js
+++ js/age/max.js
@@ -1,4 +1,4 @@
-var birth = new Date('01/01/1970'),
+var birth = new Date('05/16/1999'),
 	curr  = new Date(),
 	diff = curr.getTime() - birth.getTime(),
 	disp = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
